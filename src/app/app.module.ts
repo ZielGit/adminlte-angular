b149cb9b2304aeb78faeb9efaa61bda44728e2de
Frans <frans_421_12@hotmail.com>
2023-06-14 19:33:14 -0500

    feat: Estructura base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './pages/pages.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagesComponent,
+    DashboardComponent,
+    NopageFoundComponent
   ],
   imports: [
     BrowserModule
